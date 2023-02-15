2d0389bad9e2cef6657787f35713e53f110d7dd2
quan.thanh.vft <quan.thanh.vft@gmail.com>
2023-02-16 01:52:15 +0700

    fix: test api category and product

--- Linhkiendientu-API/Services/Categories/Dto/EditCategoryDto.cs
+++ Linhkiendientu-API/Services/Categories/Dto/EditCategoryDto.cs
@@ -6,6 +6,5 @@ namespace Linhkiendientu_API.Services.Categories.Dto
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public IsDelete IsDeleted { get; set; }
     }
 }
